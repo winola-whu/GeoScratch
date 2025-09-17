1c261a45244bd3f2f17c52a472b5bd8d47b2b2bc
张子涵 <zhangzihan@s-MacBook-Air.local>
2025-09-18 00:33:59 +1200

    feat: change rendering behavior from real-time to manual trigger with run button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,44 +1,73 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { threeObjStore } from '../BlocksCanvas/BlocksCodeGen'
 
-const Header = () => {
+
+const Header = ({ onRun }) => {
   return (
     <>
       <div className="grid grid-cols-3 gap-4 px-16 h-full items-center">
-
         <div className="flex gap-8">
-
           <div>
-            <FontAwesomeIcon icon="fa-solid fa-bars" className="text-2xl cursor-pointer hover:text-sky-700" /> 
+            <FontAwesomeIcon
+              icon="fa-solid fa-bars"
+              className="text-2xl cursor-pointer hover:text-sky-700"
+            />
           </div>
 
           <div>
-            <FontAwesomeIcon icon="fa-solid fa-angle-left" className="text-2xl cursor-pointer hover:text-sky-700"  /> 
+            <FontAwesomeIcon
+              icon="fa-solid fa-angle-left"
+              className="text-2xl cursor-pointer hover:text-sky-700"
+            />
           </div>
 
           <div>
-            <FontAwesomeIcon icon="fa-solid fa-angle-right" className="text-2xl cursor-pointer hover:text-sky-700" /> 
+            <FontAwesomeIcon
+              icon="fa-solid fa-angle-right"
+              className="text-2xl cursor-pointer hover:text-sky-700"
+            />
           </div>
 
           <div>
-            <p className='text-1xl'>Home/Entities</p>
+            <p className="text-1xl">Home/Entities</p>
           </div>
         </div>
 
-        <div className='text-center'>
-          <p className='font-bold text-3xl'>Untitled</p>
+        <div className="text-center">
+          <p className="font-bold text-3xl">Untitled</p>
         </div>
 
         <div className="flex gap-8 justify-end">
+
           <div>
-            <FontAwesomeIcon icon="fa-solid fa-gear" className="text-2xl cursor-pointer hover:text-sky-700" />
+            <FontAwesomeIcon
+              icon="fa-solid fa-play"
+              className="text-2xl cursor-pointer hover:text-sky-700"
+              onClick={() => {
+                onRun?.(Object.values(threeObjStore))
+              }}
+            />
           </div>
 
           <div>
-            <FontAwesomeIcon icon="fa-solid fa-circle-info" className='text-2xl cursor-pointer hover:text-sky-700' />
+            <FontAwesomeIcon
+              icon="fa-solid fa-gear"
+              className="text-2xl cursor-pointer hover:text-sky-700"
+            />
           </div>
 
           <div>
-            <FontAwesomeIcon icon="fa-solid fa-folder-open" className='text-2xl cursor-pointer hover:text-sky-700' />
+            <FontAwesomeIcon
+              icon="fa-solid fa-circle-info"
+              className="text-2xl cursor-pointer hover:text-sky-700"
+            />
+          </div>
+
+          <div>
+            <FontAwesomeIcon
+              icon="fa-solid fa-folder-open"
+              className="text-2xl cursor-pointer hover:text-sky-700"
+            />
           </div>
         </div>
       </div>
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -3,21 +3,31 @@ import Header from '@/components/Header/Header'
 import Scene3D from '@/components/Scene3D/Scene3D'
 import BlocksCanvas from '@/components/BlocksCanvas/BlocksCanvas'
 import { useState } from 'react'
+import { threeObjStore } from '@/components/BlocksCanvas/BlocksCodeGen'
 
 const Layout = () => {
   const [objects, setObjects] = useState([]) // [{type, params}, ...]
+  const [pendingObjects, setPendingObjects] = useState([]) // Store objects awaiting rendering
+
+  const handleRun = () => {
+    const currentObjects = Object.values(threeObjStore)
+    setPendingObjects(currentObjects)
+    setObjects(currentObjects)
+  }
+
   return (
     <>
       <div className="h-full w-full">
         <div className="h-[10%] w-full">
-          <Header />
+          <Header onRun={handleRun} />
         </div>
 
         {/* width >= 1536px : 2xl  */}
         {/* width >= 1280px : xl  */}
         {/* width >= 1024px : lg  */}
         <div className="grid 2xl:grid-cols-[40%_60%] xl:grid-cols-[40%_60%] lg:grid-cols-[50%_50%]  h-[90%]">
-          <BlocksCanvas onObjectsChange={setObjects} />
+          {/* <BlocksCanvas onObjectsChange={setObjects} /> */}
+          <BlocksCanvas onObjectsChange={setPendingObjects} />
           <Scene3D objects={objects} />
         </div>
 
